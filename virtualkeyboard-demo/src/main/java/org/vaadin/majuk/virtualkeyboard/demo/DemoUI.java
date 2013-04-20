9bdf2f0f78e3b51dbf20577089f20bc6c3aaef64
Shahim <Shahim@ml-shahim.mapna.com>
2013-04-20 16:04:16 +0300

    Kambiz Changed VirtualKeybord V1

--- virtualkeyboard-demo/src/main/java/org/vaadin/majuk/virtualkeyboard/demo/DemoUI.java
+++ virtualkeyboard-demo/src/main/java/org/vaadin/majuk/virtualkeyboard/demo/DemoUI.java
@@ -1,5 +1,7 @@
 package org.vaadin.majuk.virtualkeyboard.demo;
 
+import java.awt.TextArea;
+
 import org.vaadin.majuk.virtualkeyboard.VirtualKeyboard;
 
 import com.vaadin.annotations.Theme;
@@ -12,6 +14,7 @@ import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 
 @Theme("demo")
 @Title("VirtualKeyboard Add-on Demo")
@@ -35,22 +38,46 @@ public class DemoUI extends UI
 				else
 					virtualkeyboard.getState().current_layout = "sv";
 			}});
+    	final TextField textField1 = new TextField();
+    	final TextField textField2 = new TextField();
+    	virtualkeyboard.attachComponent(textField);
+    	virtualkeyboard.attachComponent(textField1);
+    	virtualkeyboard.attachComponent(textField2);
     	
     	virtualkeyboard.addKeyListener(new VirtualKeyboard.KeyListener() {
     			@Override	
     			public void keyPress(VirtualKeyboard.KeyEvent event) {
     				System.out.println("Key pressed: " + event.getKeyChar());
-    				textField.setValue(textField.getValue() + event.getKeyChar());
+
+    				//textField.setValue(textField.getValue() + event.getKeyChar());
+
     			}
     	});
+    	
     	
     	// Show it in the middle of the screen
     	final VerticalLayout layout = new VerticalLayout();
         layout.setStyleName("demoContentLayout");
         //layout.setSizeFull();
+    	final Window keyboardWindow= new Window();
+//        layout.setStyleName("demoContentLayout");
+//        layout.setSizeFull();
+
+//       keyboardWindow.setPositionX(200);
+//       keyboardWindow.setPositionY(100);
+//       keyboardWindow.setWidth("600");
+//       keyboardWindow.setHeight("400");
+//       addWindow(keyboardWindow);
+
         layout.addComponent(virtualkeyboard);
         layout.addComponent(textField);
+
         layout.addComponent(switchbutton);
+
+        layout.addComponent(textField1);
+        layout.addComponent(textField2);
+        //layout.addComponent(new TextArea());
+
         setContent(layout);
 
     }
--- virtualkeyboard/src/main/java/org/vaadin/majuk/virtualkeyboard/VirtualKeyboard.java
+++ virtualkeyboard/src/main/java/org/vaadin/majuk/virtualkeyboard/VirtualKeyboard.java
@@ -8,11 +8,17 @@ import org.json.JSONException;
 import org.vaadin.majuk.virtualkeyboard.VirtualKeyboard.KeyListener;
 
 import com.vaadin.annotations.JavaScript;
+import com.vaadin.event.FieldEvents.FocusEvent;
+import com.vaadin.event.FieldEvents.FocusListener;
 import com.vaadin.shared.MouseEventDetails;
 import com.vaadin.shared.communication.ServerRpc;
 import com.vaadin.shared.ui.JavaScriptComponentState;
+import com.vaadin.ui.AbstractComponent;
+import com.vaadin.ui.AbstractTextField;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Notification;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.Component.Event;
 
 // This is the server-side UI component that provides public API 
 // for VirtualKeyboard
@@ -20,7 +26,9 @@ import com.vaadin.ui.Notification;
 public class VirtualKeyboard extends com.vaadin.ui.AbstractJavaScriptComponent {
 	
 	List<KeyListener> listenerList;
-
+	List<AbstractTextField> components;
+	private AbstractTextField focusedTextField;
+	
 	public interface KeyListener {
 		void keyPress(VirtualKeyboard.KeyEvent event);
 	}
@@ -45,9 +53,27 @@ public class VirtualKeyboard extends com.vaadin.ui.AbstractJavaScriptComponent {
 
 	public VirtualKeyboard(){
 		listenerList = new ArrayList<KeyListener>();
+		components = new ArrayList<AbstractTextField>();
 				
         registerRpc(new KeyClickRpc() {
             public void onKeyClick(String s) {
+                //Notification.show("Clicked on [" + s + "]");
+            	if(focusedTextField != null)
+            	{
+            		int curPos = focusedTextField.getCursorPosition();
+            		String oldText = focusedTextField.getValue();
+            		if(s != null && s == "{bksp}") {
+            			focusedTextField.setValue(oldText.substring(0,curPos-1) + oldText.substring(curPos,oldText.length()));
+            		}
+            		else {
+            			focusedTextField.setValue(oldText.substring(0,curPos) + s + oldText.substring(curPos,oldText.length()));
+            			focusedTextField.setCursorPosition(curPos+1);
+            		}
+
+            		
+            		focusedTextField.focus();
+            	}
+            	
                 for (KeyListener keyListener : listenerList) {
 					keyListener.keyPress(new KeyEvent(VirtualKeyboard.this, s));
 				}
@@ -71,5 +97,22 @@ public class VirtualKeyboard extends com.vaadin.ui.AbstractJavaScriptComponent {
 
 	public void removeKeyListener(KeyListener keyListener) {
 		listenerList.remove(keyListener);
+	}
+	
+	public void attachComponent(AbstractTextField component) {
+		components.add(component);
+		
+		component.addFocusListener(new FocusListener() {
+			
+			@Override
+			public void focus(FocusEvent event) {
+				focusedTextField = (AbstractTextField) event.getComponent();
+				
+			}
+		});
+	}
+	
+	public void dettachComponent(AbstractTextField component) {
+		components.remove(component);
 	}
 }
