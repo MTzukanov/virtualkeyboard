91968db850dd1c22a99c7d2988e04d50587cd8ac
Michael.Tzukanov <Michael.Tzukanov@abo.fi>
2013-04-20 16:33:30 +0300

    Fixed keyboard

--- virtualkeyboard/src/main/resources/org/vaadin/majuk/virtualkeyboard/keyboards.js
+++ virtualkeyboard/src/main/resources/org/vaadin/majuk/virtualkeyboard/keyboards.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-var key_value_dict = { 'sv': {
-=======
 var key_value_dict = { 'default': {
->>>>>>> master
 	'{shift}': "Shift",
 	'{alt}': "AltGr",
 	'{enter}': "Enter",
@@ -14,31 +10,6 @@ var key_value_dict = { 'default': {
 };
 
 /* qwerty by Mika Perreri Korhonen (https://github.com/jouk0) - */
-<<<<<<< HEAD
-var keyboards = { 'sv': {
-'default' : [
-"\u00a7 1 2 3 4 5 6 7 8 9 0 + \u0301 {bksp}",
-"{tab} q w e r t y u i o p \u00e5 \u0308",
-"{cplk} a s d f g h j k l \u00f6 \u00e4 ' {enter}",
-"{shift} < z x c v b n m , . - {shift}",
-"{alt} {space} {alt}"
-],
-'shift' : [
-'\u00bd ! " # \u00a4 % & / ( ) = ? \u0300 {bksp}',
-"{tab} Q W E R T Y U I O P \u00c5 \u0302",
-"{cplk} A S D F G H J K L \u00d6 \u00c4 * {enter}",
-"{shift} > Z X C V B N M ; : _ {shift}",
-"{alt} {space} {alt}"
-],
-'alt' : [
-'\u00a7 1 @ \u00a3 $ 5 6 { [ ] } \\ \u0301 {bksp}',
-'{tab} q w € r t y u i o p \u00e5 \u0303',
-"{cplk} a s d f g h j k l \u00f6 \u00e4 ' {enter}",
-'{shift} | z x c v b n \u00b5 , . - {shift}',
-'{alt} {space} {alt}'
-]
-} };
-=======
 var keyboards = {
 'sv': {
 	'default' : [
@@ -88,4 +59,3 @@ var keyboards = {
 	]
 	},
 };
->>>>>>> master
